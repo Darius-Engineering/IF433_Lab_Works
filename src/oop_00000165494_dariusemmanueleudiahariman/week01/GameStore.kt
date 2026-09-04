c638fac19cedadb7e6402bd9b387a84cbe588c50
Darsui-AFK <dariusemmanuel.e.h@gmail.com>
2026-09-04 18:04:28 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000165494_dariusemmanueleudiahariman/week01/GameStore.kt
+++ src/oop_00000165494_dariusemmanueleudiahariman/week01/GameStore.kt
@@ -3,6 +3,20 @@ package oop_00000165494_dariusemmanueleudiahariman.week01
 fun main(){
     val gameTitle = "Elden Ring"
     val price = 599000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    println("Price: $price")
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int){
+    println("Game Title = $title")
+    println("Final Price = Rp. $finalPrice")
+}
+
+
+
