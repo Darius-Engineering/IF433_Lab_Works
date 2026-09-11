c8058703459b526ea44a086235de402c0cf0d0d8
Darsui-AFK <dariusemmanuel.e.h@gmail.com>
2026-09-11 21:00:35 +0700

    week02: (task 1) implement library input system

---
+++ src/oop_00000165494_dariusemmanueleudiahariman/week02/MainLoan.kt
@@ -0,0 +1,32 @@
+package oop_00000165494_dariusemmanueleudiahariman.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- Sistem Denda Perpustakaan ---")
+    print("Masukkan Judul Buku: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam: ")
+    var loanDuration = scanner.nextInt()
+
+    scanner.nextLine()
+
+    if (loanDuration < 0) {
+        println("ERROR: Diubah menjadi 1 hari. Lama pinjam tidak boleh minus!")
+        loanDuration = 1
+    }
+
+    val loan = Loan(bookTitle, borrower, loanDuration)
+
+    println("--- Detail Peminjaman ---")
+    println("Judul Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Lama Pinjam: ${loan.loanDuration}")
+    println("Total Denda: Rp. ${loan.calulateFine()}")
+}
