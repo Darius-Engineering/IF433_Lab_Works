a52b4d9619df0a71ebb144168c3423a451639083
Darsui-AFK <dariusemmanuel.e.h@gmail.com>
2026-09-12 09:59:47 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000165494_dariusemmanueleudiahariman/week03/Employee.kt
+++ src/oop_00000165494_dariusemmanueleudiahariman/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000165494_dariusemmanueleudiahariman.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000165494_dariusemmanueleudiahariman/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000165494_dariusemmanueleudiahariman.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
